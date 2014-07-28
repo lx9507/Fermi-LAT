1822656c7b5365260db4d4ad993bed0f2f138f3d
Li, Xiang <xiangli@pmo.ac.cn>
2014-07-28 16:24:29 +0800

    Using make2FGLxml package to generate the source model file

---
+++ model.py
@@ -0,0 +1,15 @@
+# For P7REP_SOURCE_V15 data, ST version v9r33p0
+from make2FGLxml import *
+import os
+import commands as cmd
+
+srcfile = cmd.getoutput(". ./var_common.gts && echo $file_filtered_gti")
+catalog_dir = os.environ.get('HOME') + '/data_Fermi/catalog'
+mymodel = srcList(catalog_dir + '/gll_psc_v08.fit',
+                  srcfile, 'model.xml')
+pathtodiffusefiles = os.environ.get('FERMI_DIR') + '/refdata/fermi/galdiffuse'
+mymodel.makeModel(pathtodiffusefiles + '/gll_iem_v05_rev1.fits',
+                  'gll_iem_v05_rev1',
+                  pathtodiffusefiles + '/iso_source_v05_rev1.txt',
+                  'iso_source_v05',
+                  extDir=catalog_dir + '/Templates')
